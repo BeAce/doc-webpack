64618c7b29e46ec6d668d9fbcb74ad83ef032bf0
BeAce <beaceshimin@gmail.com>
2017-05-31 17:33:19 +0800

    feat: doc-2 how to config webpack

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-var map = require('lodash/map')
+import { map } from 'lodash'
 
 function square(n) {
   return n * n
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+var path = require("path");
+
+module.exports = {
+  entry: "./src/main.js",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: { plugins: ["transform-runtime"], presets: ["es2015"] }
+      }
+    ]
+  }
+};
